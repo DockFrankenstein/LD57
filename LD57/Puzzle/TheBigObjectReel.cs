665577ffe08c1dce15546e38087caf3e7d56ec43
Dock Frankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2025-04-07 16:03:14 +0200

    Added indicators to big obj reels

--- LD57/Puzzle/TheBigObjectReel.cs
+++ LD57/Puzzle/TheBigObjectReel.cs
@@ -1,14 +1,21 @@
 ﻿using LD57.Interaction;
+using Stride.Rendering;
 
 namespace LD57.Puzzle
 {
     public class TheBigObjectReel : StartupScript, IInteractable
     {
+        const float IND_ROOT_Y = -3f;
+
         public TheBigObject Target { get; set; }
         public List<bool> StateAvaliability { get; set; } = new List<bool>();
 
         public bool Interactable { get; set; } = true;
         [DataMemberIgnore] public bool Focused { get; set; }
+
+        public Model IndicatorModel { get; set; }
+        public Material IndMatOn { get; set; }
+        public Material IndMatOff { get; set; }
 
         public void Interact()
         {
@@ -36,5 +43,52 @@ namespace LD57.Puzzle
 
         bool Avaliable(int index) =>
             index >= 0 && (StateAvaliability.Count <= index || StateAvaliability[index]);
+
+        Entity indRoot;
+        List<Indicator> Indicators = new List<Indicator>();
+
+        public override void Start()
+        {
+            indRoot = new Entity(position: new Vector3(0f, IND_ROOT_Y + 2f, 0f));
+            Entity.AddChild(indRoot);
+
+            if (Target != null)
+            {
+                var rollOffset = -GetRollForIndex((Target.States.Count - 1) / 2f);
+
+                for (int i = 0; i < Target.States.Count; i++)
+                {
+                    var ind = new Indicator()
+                    {
+                        pivot = new Entity(
+                            rotation: Quaternion.RotationYawPitchRoll(0f, 0f, rollOffset + GetRollForIndex(i))),
+                        
+                        model = new ModelComponent(IndicatorModel),
+                    };
+
+                    var modelEntity = new Entity(
+                        position: new Vector3(0f, -IND_ROOT_Y, 0f),
+                        rotation: Quaternion.RotationYawPitchRoll(0f, -30f / 180f * (float)Math.PI, 0f));
+                    modelEntity.Add(ind.model);
+
+                    ind.model.Materials.Clear();
+                    ind.model.Materials.Add(0, Avaliable(i) ? IndMatOn : IndMatOff);
+
+                    ind.pivot.AddChild(modelEntity);
+                    indRoot.AddChild(ind.pivot);
+
+                    Indicators.Add(ind);
+                }
+            }
+        }
+
+        float GetRollForIndex(float i) =>
+            i * (float)Math.PI * 2f * 0.03f;
+
+        class Indicator
+        {
+            public Entity pivot;
+            public ModelComponent model;
+        }
     }
 }
